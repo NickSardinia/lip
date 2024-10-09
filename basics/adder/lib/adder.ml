bc549aa186fcb6d49e0d305ce064a4947fe68def
NickSardinia <pittau18@gmail.com>
2024-10-09 15:58:51 +0200

    Create function addlist in basics/adder

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,8 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+(*let addlist l = 0 *)(* replace 0 with actual code *)
+
+let rec addlist = function
+    [] -> 0
+  | a::l -> a + (addlist l);;
